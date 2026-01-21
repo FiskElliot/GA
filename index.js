c0bf4b764292d9af9742146cc8a8974d551101cd
Elliot Jägstedt <jagstedtellit@gmail.com>
2026-01-21 12:14:33 +0100

    Fixade lite ändringar

---
+++ index.js
@@ -0,0 +1,14 @@
+// Nytt projekt: npm init -y i terminalen
+// Installera express: npm i express i terminalen
+// Sätta igång program om filen heter index.js: node --watch index
+// --------------------------------------------------------------------
+const express = require("express");
+
+const app = express();
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
+app.get("/", (req, res) => {
+    res.send("HOME");
+});
