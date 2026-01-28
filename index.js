72bfab3642bd19253ed0704079461c96596c83b4
FiskElliot <jagstedtelliot@gmail.com>
2026-01-28 12:27:49 +0100

    Testade lite olika funktioner i PUG

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ app.listen(3000, () => {console.log("Server running on http://localhost:3000");}
 app.set("view engine", "pug")
 
 app.get("/", (req, res) => {
+    res.render("template");
+});
+
+app.get("/index", (req, res) => {
     res.render("index");
 });
