0bc641ddbf8191162d794ede4cdc4cd2f6aa9347
FiskElliot <jagstedtelliot@gmail.com>
2026-03-24 12:05:28 +0100

    lite css, ska göra login/register

--- db.js
+++ db.js
@@ -1,15 +1,3 @@
-/* const fs = require("fs");
-
-function saveData(data){
-    fs.writeFileSync("db.json", JSON.stringify(data, null, 3))
-}
-
-function getData(){
-    return JSON.parse(fs.readFileSync("db.json").toString())
-}
-
-module.exports = {getData, saveData} */
-
 const fs = require('fs');
 
 function saveData(data, filename = 'db.json') {
@@ -19,5 +7,11 @@ function saveData(data, filename = 'db.json') {
 function getData(filename = 'db.json') {
     return JSON.parse(fs.readFileSync(filename).toString());
 }
+
+
+
+
+
+
 
 module.exports = { getData, saveData };
